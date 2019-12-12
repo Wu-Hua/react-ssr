dd372cade4c825242a659d2dabc0cc3f8ad4ddbd
吴华 <569405489@qq.com>
2019-12-12 15:50:13 +0800

    redux同构支持

--- client/index.js
+++ client/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import App from '../src/App'
+import store from '../src/store/store'
 
 // 注水 客户端入口
 const Page = (
-  <BrowserRouter>
-    {App}
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      {App}
+    </BrowserRouter>
+  </Provider>
 )
-ReactDom.hydrate(Page,document.getElementById('root'))
+ReactDom.hydrate(Page, document.getElementById('root'))
--- server/index.js
+++ server/index.js
@@ -1,21 +1,25 @@
 // 这里的node代码，会用babel处理
 import React from 'react'
-import {renderToString} from 'react-dom/server'
+import { renderToString } from 'react-dom/server'
 import express from 'express'
-import {StaticRouter} from 'react-router-dom'
+import { StaticRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from '../src/store/store'
 import App from '../src/App'
 
 const app = express()
 app.use(express.static('public'))
 
-app.get('*',(req,res)=>{
+app.get('*', (req, res) => {
 
   // const Page = <App title='kaideba'></App>
   // 个react组件，解析成html
   const content = renderToString(
-    <StaticRouter location={req.url}>
-      {App}
-    </StaticRouter>
+    <Provider store={store}>
+      <StaticRouter location={req.url}>
+        {App}
+      </StaticRouter>
+    </Provider>
   )
   res.send(`
     <html>
@@ -31,6 +35,6 @@ app.get('*',(req,res)=>{
   `)
 })
 
-app.listen(8080,_=>{
+app.listen(8080, _ => {
   console.log('监听完毕')
 })
